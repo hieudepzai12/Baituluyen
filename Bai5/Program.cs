8711764b3914c19711fc0932c136bb56b485c365
ADMIN <ADMIN@PC348>
2024-01-05 11:12:29 +0700

    Add project files.

---
+++ Bai5/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            menu();
+        }
+        static void menu()
+        {
+            QuanlyCD quanlyCD = new QuanlyCD();
+            int chon = 0;
+            do
+            {
+                Console.WriteLine("**********Chuong trinh quan ly CD*********");
+                Console.WriteLine("1. Them CD.\n2. Tinh gia thanh trung binh.\n3. Sap xep CD giam dan them gia thanh.\n4. Sap xep CD tang dan theo tua CD.\n5. Xuat toan bo CD.\n0. Thoat chuong trinh.");
+                Console.WriteLine("-----------------------------------------");
+                Console.Write("Ban chon:");
+                chon = int.Parse(Console.ReadLine());
+
+                switch (chon)
+                {
+                    case 1:
+                        CD cd = new CD();
+                        Console.Write("Nhap ma CD:");
+                        cd.MaCD = int.Parse(Console.ReadLine());
+                        Console.Write("Nhap tua CD:");
+                        cd.TuaCD = Console.ReadLine();
+                        Console.Write("Nhap ten ca si:");
+                        cd.CaSi = Console.ReadLine();
+                        Console.Write("Nhap so bai hat da hat:");
+                        cd.SoBaiHat = int.Parse(Console.ReadLine());
+                        Console.Write("Nhap gia tien:");
+                        cd.DonGia = int.Parse(Console.ReadLine());
+                        quanlyCD.ThemCD(cd);
+                        break;
+
+                    case 2:
+                        double kq = quanlyCD.TinhGiaTB();
+                        Console.WriteLine("Gia tien trung binh: {0:#,##0.00}", kq);
+                        break;
+
+                    case 3:
+                        quanlyCD.SapXepGiamTheoGiaThanh();
+                        Console.WriteLine("Da sap them theo gia tien giam dan!");
+                        break;
+
+                    case 4:
+                        quanlyCD.SapXepTangTheoTuaCD();
+                        Console.WriteLine("Da sap xep theo tua CD!");
+                        break;
+
+                    case 5:
+                        quanlyCD.XuatGiaTri();
+                        break;
+
+                    case 0:
+                        Console.WriteLine("Goodbye! Don't see you again!");
+                        Console.ReadLine();
+                        break;
+                }
+            } while (chon != 0);
+        }
+    }
+}
+        
---
+++ Bai5/QuanlyCD.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai5
+{
+    class QuanlyCD
+    {
+        private CD[] ds;
+        private int n; 
+        public QuanlyCD()
+        {
+            ds = new CD[100];
+            n = 0;
+        }
+        public QuanlyCD(int sophantu)
+        {
+            ds = new CD[sophantu];
+            n = 0;
+        }
+
+        public void ThemCD(CD cd)
+        {
+            if (n >= ds.Length)
+            {
+                Console.WriteLine("Danh sach khong con cho trong!");
+            }
+            else
+            {
+                if (!KiemTraTrungCD(cd.MaCD))
+                    ds[n++] = cd;
+                else
+                {
+                    Console.WriteLine("Trung ma CD!");
+                }
+            }
+        }
+
+        private bool KiemTraTrungCD(int macd)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (ds[i].MaCD == macd)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public double TinhGiaTB()
+        {
+            int tonggia = 0;
+            for (int i = 0; i < n; i++)
+            {
+                tonggia += ds[i].DonGia;
+            }
+            return (double)tonggia / n;
+        }
+
+        public void XuatGiaTri()
+        {
+            Console.WriteLine("{0, 10} {1,30} {2,30} {3,10} {4, 15}",
+            "MaCD", "Tua CD", "Ca si", "So bai hat", "Gia tien");
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(ds[i].ToString());
+            }
+        }
+
+        public void SapXepGiamTheoGiaThanh()
+        {
+            CD tam;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (ds[i].DonGia < ds[j].DonGia)
+                    {
+                        tam = ds[i];
+                        ds[i] = ds[j];
+                        ds[j] = tam;
+                    }
+                }
+            }
+        }   
+        
+        public void SapXepTangTheoTuaCD()
+        {
+            CD tam;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (ds[i].TuaCD.CompareTo(ds[j].TuaCD)>0)
+                    {
+                        tam = ds[i];
+                        ds[i] = ds[j];
+                        ds[j] = tam;
+                    }
+                }
+            }
+        }
+    }
+}
